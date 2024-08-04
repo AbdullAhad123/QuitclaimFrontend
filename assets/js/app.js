633bb0c2d63c2d81a4876589970e804967435674
abdulahad <124651992+AbdullAhad123@users.noreply.github.com>
2024-08-04 23:55:53 +0500

    add Login page and preloader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+let $loader = $("#preloaderCont");
+
+$(window).on("load", function () {
+  setTimeout(function () {
+    $loader.fadeOut();
+  }, 2000);
+});
+
 $(".user_btn").on("click", function () {
   const dropdown = $(this).siblings(".user_details_menu");
   const icon = $(this).find("svg");
@@ -5,17 +13,17 @@ $(".user_btn").on("click", function () {
   icon.toggleClass("dropdownIconTransform");
 });
 
-// show backToTop 
-$(window).on('scroll load', function () {
-    const scrolled = $(window).scrollTop();
-    if (scrolled >= 500) {
-        $("#topBtn").fadeIn()
-    } else {
-        $("#topBtn").fadeOut()
-    }
+// show backToTop
+$(window).on("scroll load", function () {
+  const scrolled = $(window).scrollTop();
+  if (scrolled >= 500) {
+    $("#topBtn").fadeIn();
+  } else {
+    $("#topBtn").fadeOut();
+  }
 });
 
 // backToTop
 $("#backToTop").click(function () {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 });
